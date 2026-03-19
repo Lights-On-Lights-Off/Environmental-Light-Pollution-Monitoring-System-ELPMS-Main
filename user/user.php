90a8d261ce33f7fe6a864d3b5f25eff3c4f25cff
RjayBernales <rainieljevson0@gmail.com>
2026-03-19 12:01:11 +0800

    fixed the dropdown back to home not functioning

--- user/user.php
+++ user/user.php
@@ -69,10 +69,10 @@ function timeAgo(string $datetime): string {
                 <div class="dropdown-user-name"><?= htmlspecialchars($user['name']) ?></div>
                 <div class="dropdown-user-email"><?= htmlspecialchars($user['email']) ?></div>
             </div>
-            <button class="dropdown-item-btn" onclick="window.location.href="<?= url('index.php') ?>"">
+            <a href="<?= url('user/user.php') ?>" class="dropdown-item-btn" style="text-decoration:none;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" viewBox="0 0 16 16"><path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-4h3v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L8.354 1.146z"/></svg>
                 Back to Home
-            </button>
+            </a>
             <a href="<?= url('logout.php') ?>" class="dropdown-item-btn danger" style="text-decoration:none;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/><path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg>
                 Logout
@@ -278,6 +278,8 @@ const BUILDINGS_DATA = <?= json_encode(array_map(fn($b) => [
     'coordinates'    => [(float)$b['lat'], (float)$b['lng']],
     'pollutionLevel' => $b['pollution_level'],
     'description'    => $b['description'] ?? '',
+    'lux'            => (float)$b['lux'],
+    'online'         => (bool)$b['online'],
 ], $buildings), JSON_HEX_TAG) ?>;
 </script>
 <script src="<?= url('assets/js/user.js') ?>"></script>
